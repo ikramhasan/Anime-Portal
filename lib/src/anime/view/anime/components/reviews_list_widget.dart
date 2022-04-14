46fa438d198feb25a7835e4a4e3e5de5ba0fa326
Ikramul Hasan <ikramhasan.dev@gmail.com>
2022-04-14 15:01:23 +0600

    added: empty state

--- lib/src/anime/view/anime/components/reviews_list_widget.dart
+++ lib/src/anime/view/anime/components/reviews_list_widget.dart
@@ -16,56 +16,58 @@ class ReviewsListWidget extends StatelessWidget {
         } else if (state.reviews.isNotEmpty) {
           return Padding(
             padding: const EdgeInsets.symmetric(horizontal: 16),
-            child: ListView.builder(
-              shrinkWrap: true,
-              itemCount: state.reviews.length,
-              itemBuilder: (context, index) {
-                final review = state.reviews[index];
-                return Card(
-                  child: Padding(
-                    padding: const EdgeInsets.all(8),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Row(
-                          children: [
-                            CircleAvatar(
-                              radius: 10,
-                              backgroundImage: NetworkImage(
-                                review.reviewer.imageUrl,
+            child: state.reviews.isEmpty
+                ? const Text('No reviews yet')
+                : ListView.builder(
+                    shrinkWrap: true,
+                    itemCount: state.reviews.length,
+                    itemBuilder: (context, index) {
+                      final review = state.reviews[index];
+                      return Card(
+                        child: Padding(
+                          padding: const EdgeInsets.all(8),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Row(
+                                children: [
+                                  CircleAvatar(
+                                    radius: 10,
+                                    backgroundImage: NetworkImage(
+                                      review.reviewer.imageUrl,
+                                    ),
+                                  ),
+                                  const SizedBox(width: 8),
+                                  Text(
+                                    review.reviewer.username,
+                                    style: const TextStyle(fontSize: 12),
+                                  ),
+                                  const Spacer(),
+                                  AnimeRatingBar(
+                                    rating: review.reviewer.reviewScore.overall
+                                        .toDouble(),
+                                  ),
+                                ],
                               ),
-                            ),
-                            const SizedBox(width: 8),
-                            Text(
-                              review.reviewer.username,
-                              style: const TextStyle(fontSize: 12),
-                            ),
-                            const Spacer(),
-                            AnimeRatingBar(
-                              rating: review.reviewer.reviewScore.overall
-                                  .toDouble(),
-                            ),
-                          ],
-                        ),
-                        const Divider(),
-                        Text(
-                          review.content,
-                          style: const TextStyle(fontSize: 13),
-                        ),
-                        const Divider(),
-                        Text(
-                          '${review.helpfulCount} people found this review helpful',
-                          style: TextStyle(
-                            fontSize: 13,
-                            color: Theme.of(context).disabledColor,
+                              const Divider(),
+                              Text(
+                                review.content,
+                                style: const TextStyle(fontSize: 13),
+                              ),
+                              const Divider(),
+                              Text(
+                                '${review.helpfulCount} people found this review helpful',
+                                style: TextStyle(
+                                  fontSize: 13,
+                                  color: Theme.of(context).disabledColor,
+                                ),
+                              ),
+                            ],
                           ),
                         ),
-                      ],
-                    ),
+                      );
+                    },
                   ),
-                );
-              },
-            ),
           );
         } else {
           return const SizedBox.shrink();
